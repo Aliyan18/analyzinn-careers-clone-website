c8187a9cdb250bccf531177990985f34a3aaeae1
iznahasim <96913016+iznahasim@users.noreply.github.com>
2024-08-07 15:44:12 -0700

    dd

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   console.log("iznah workking")
+  console.log("iznah workking")
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
