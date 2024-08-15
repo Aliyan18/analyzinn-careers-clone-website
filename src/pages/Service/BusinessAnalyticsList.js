9d6ff71a3a7435803022e2aeed2add2f806e1495
Aliyan18 <rajaaliyanahmed@gmail.com>
2024-08-15 09:30:38 -0700

    final commit

--- src/pages/Service/BusinessAnalyticsList.js
+++ src/pages/Service/BusinessAnalyticsList.js
@@ -2,11 +2,12 @@ import React from 'react'
  import "../../styleSheets/BusinessAnalyticsList.css"
 
 const ServiceList = ({title, description}) => {
-  return (
+  return (<>
     <div className="serviceba-card">
     <h2 className="serviceba-title">{title}</h2>
     <p className="serviceba-description">{description}</p>
   </div>
+  </>
   )
 }
 
